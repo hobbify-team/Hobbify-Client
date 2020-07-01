9b7cef367e1143fb6d8c138bffa6df849313facd
iamyoujared <jaredortega2715@gmail.com>
2020-07-01 10:32:18 -0500

    Set new config proyect, register form was added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from "react";
 import { GlobalStyles } from "./styles";
+import { Button, Input } from "antd";
 
 export const App = () => {
   return (
     <Fragment>
       <GlobalStyles />
+      <Button type="primary">Primary</Button>
+      <Input placeholder="Basic usage" />
       <h2>Start proyect</h2>
     </Fragment>
   );
